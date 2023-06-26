32646c20a01c9e6ecbb74fbba93bb2671cff0180
AzaanDev <xxx.azaan.ali@gmail.com>
2023-06-26 13:36:29 -0400

    project update

--- config/site.ts
+++ config/site.ts
@@ -11,7 +11,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    linkedin: "https://twitter.com/shadcn",
+    linkedin: "https://www.linkedin.com/in/azaan-ali/",
     github: "https://github.com/AzaanDev",
   },
   owner: {
@@ -21,15 +21,20 @@ export const siteConfig = {
   },
   repos: [
     {
-      name: "Chip8 Emulator",
-      full_name: "AzaanDev/Chip8",
-      html_url: "https://github.com/AzaanDev/Chip8",
-      description: "Chip8 Emulator",
+      name: "Bomberman",
+      full_name: "AzaanDev/Bomberman",
+      html_url: "https://github.com/AzaanDev/Bomberman",
+      description:
+        "Implemented an multiplayer browser game inspired by Bomberman, featuring real-time gameplay, dynamic multiplayer interactions, and a leaderboard system.",
       languages: {
         nodes: [
           {
-            name: "C++",
-            color: "#ff6666",
+            name: "C#",
+            color: "#66c2a5",
+          },
+          {
+            name: "TypeScript",
+            color: "#3288bd",
           },
         ],
       },
@@ -38,12 +43,17 @@ export const siteConfig = {
       name: "Algorithms Visualizer",
       full_name: "AzaanDev/cs_project",
       html_url: "https://github.com/AzaanDev/cs_project",
-      description: "Chip8 Emulator",
+      description:
+        "Developed a visualizer showcasing various algorithms & data structures in action, providing a dynamic and intuitive platform to explore and understand their functionality.",
       languages: {
         nodes: [
           {
-            name: "C++",
-            color: "#ff6666",
+            name: "JavaScript",
+            color: "#e6f598",
+          },
+          {
+            name: "Python",
+            color: "#fdae61",
           },
         ],
       },
@@ -71,7 +81,8 @@ export const siteConfig = {
       name: "Game Engine",
       full_name: "AzaanDev/S22_Azaan_Ali",
       html_url: "https://github.com/AzaanDev/S22_Azaan_Ali",
-      description: "Chip8 Emulator",
+      description:
+        "Developed a game engine utilizing OpenGL, showcasing a passion for game development and a comprehensive understanding of graphics rendering and game design principles.",
       languages: {
         nodes: [
           {
@@ -85,26 +96,36 @@ export const siteConfig = {
       name: "Social Media App",
       full_name: "AzaanDev/SocialMediaApp",
       html_url: "https://github.com/AzaanDev/SocialMediaApp",
-      description: "Chip8 Emulator",
+      description:
+        "Created an platform for social connections and content sharing, designed with intuitive interfaces and personalized feeds.",
       languages: {
         nodes: [
           {
-            name: "C++",
-            color: "#ff6666",
+            name: "TypeScript",
+            color: "#3288bd",
           },
         ],
       },
     },
     {
-      name: "TCP Chat",
-      full_name: "AzaanDev/TCP-Chat",
-      html_url: "https://github.com/AzaanDev/TCP-Chat",
-      description: "Chip8 Emulator",
+      name: "Discord Clone",
+      full_name: "AzaanDev/Real-Time-Chat-App",
+      html_url: "https://github.com/AzaanDev/Real-Time-Chat-App",
+      description:
+        "Created a real-time chat application, mimicking the microservice infrastructure of Discord, facilitating seamless communication among groups of users.",
       languages: {
         nodes: [
           {
-            name: "C++",
-            color: "#ff6666",
+            name: "C#",
+            color: "#66c2a5",
+          },
+          {
+            name: "TypeScript",
+            color: "#3288bd",
+          },
+          {
+            name: "JavaScript",
+            color: "#e6f598",
           },
         ],
       },
